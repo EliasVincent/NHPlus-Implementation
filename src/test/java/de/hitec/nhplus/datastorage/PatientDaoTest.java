9cae9a977e71220fe98ea514fca6340c37793403
Ayoub-aalaou97 <aalaou.ayoub97@gmail.com>
2024-06-01 14:21:45 +0200

    Unit-Test zur Ueberpruefung des Nichtvorhandenseins eines Vermoegensstands in der Patiententabelle

---
+++ src/test/java/de/hitec/nhplus/datastorage/PatientDaoTest.java
@@ -0,0 +1,56 @@
+package de.hitec.nhplus.datastorage;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PatientDaoTest {
+
+    private static Connection connection;
+
+    /**
+     * Initialisiert Verbindung zur Datenbank.
+     */
+    @BeforeEach
+    public void setUp() {
+        connection = ConnectionBuilder.getConnection();
+    }
+
+    /**
+     * Schließt die Verbindung zur Datenbank, nachdem alle Tests ausgeführt wurden.
+     */
+    @AfterAll
+    public static void closeConnection() {
+        ConnectionBuilder.closeConnection();
+    }
+
+    /**
+     * Überprüft, ob eine bestimmte Spalte in der Tabelle "Patient" vorhanden ist.
+     * Wenn die Spalte nicht existiert, wird der Test bestanden.
+     * Wenn die Spalte existiert, wird der Test nicht bestanden.
+     */
+    @Test
+    public void testIfColumnExistsInPatientTable() {
+        String columnName = "assets";
+        try {
+            DatabaseMetaData metaData = connection.getMetaData();
+            ResultSet resultSet = metaData.getColumns(null, null, "patient", columnName);
+            boolean columnExists = resultSet.next();
+            resultSet.close();
+
+            if (columnExists) {
+                assertFalse(true, "Die Spalte existiert in der 'patient' Tabelle.");
+            } else {
+                assertTrue(true,"Die Spalte existiert nicht in der 'patient' Tabelle");
+            }
+        } catch (SQLException e) {
+            fail("SQLException occurred: " + e.getMessage());
+        }
+    }
+}
